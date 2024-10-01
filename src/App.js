79176c696ddceea15f3af54ebcd32309d707ab7f
Martin Bradbury <martinbradbury@btinternet.com>
2024-10-01 14:41:49 +0100

    added routing in app.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className={styles.App}>
       <NavBar />
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route exact path='/' element={<Home />} />
         <Route path='*' element={<PageNotFound />} />
       </Routes>
     </div>
