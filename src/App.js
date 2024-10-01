95eae3b66f3e154faaa7baa68c71c351c8823763
MartinBradburyPC <you@example.com>
2024-10-01 23:09:04 +0100

    updated database in axiosDefaults

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import NavBar from './components/NavBar';
 import {Routes, Route} from 'react-router-dom'
 import Home from './pages/home/Home';
 import PageNotFound from './pages/pagenotfound/PageNotFound';
+import './api/axiosDefaults';
+import SignUpForm from './pages/auth/SignUpForm';
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
       <Routes>
-        <Route exact path='/' element={<Home />} />
+        <Route exact path='/home' element={<Home />} />
         <Route path='*' element={<PageNotFound />} />
+        <Route path='/signup' element={<SignUpForm />} />
       </Routes>
     </div>
   );
